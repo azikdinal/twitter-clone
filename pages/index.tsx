6a0c04b2c61e24a1304230d24d25b971228796b7
azamat <dinaev2002@gmail.com>
2023-07-15 16:54:11 +0300

    wq

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import SearchBar from "@/components/SearchBar"
+import Feed from "@/components/Feed"
+import Sidebar from "@/components/Sidebar"
+
 export default function Home() {
   return (
-    <div className='text-3xl text-sky-500'>
-      Hello
+    <div className="h-screen bg-black">
+     <div className="grid grid-cols-4 h-full">
+      <Sidebar />
+      <Feed />
+      <SearchBar />
+     </div>
     </div>
   )
 }
