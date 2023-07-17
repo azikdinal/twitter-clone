9e1ef36394104daa67d56563e5f51848de1185fd
azamat <dinaev2002@gmail.com>
2023-07-17 21:48:02 +0300

    2.10 loader error

---
+++ components/users/UserHero.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserHero = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UserHero;
---
+++ pages/users/[userId].tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from "@/components/Header";
+import {useRouter} from "next/router";
+import useUser from "@/hooks/useUser";
+import {ClipLoader} from "react-spinners";
+import UserHero from "@/components/users/UserHero";
+
+const UserView = () => {
+    const router = useRouter()
+    const {userId} = router.query
+
+    const {data: fetchedUser, isLoading} = useUser(userId as string)
+
+    if (isLoading || !fetchedUser) {
+        return (
+            <div className="
+                flex
+                justify-center
+                items-center
+                h-full
+            ">
+                <ClipLoader color='lightblue' size={80}/>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <Header showBackArrow label={fetchedUser?.name}/>
+            <UserHero userId={userId as string}/>
+        </>
+    );
+};
+
+export default UserView;
